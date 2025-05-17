89ad2f667389e0b6bf09b17071b8a99df5f17a17
ninabarzh <30133030+ninabarzh@users.noreply.github.com>
2025-05-17 12:05:52 +0200

    _headers file

--- source/conf.py
+++ source/conf.py
@@ -51,6 +51,7 @@ html_favicon = "img/favicon.ico"
 html_static_path = ['_static']
 html_css_files = ['css/custom.css']
 html_last_updated_fmt = '%Y-%m-%d %H:%M'  # e.g., "May 05, 2025 at 14:30"
+html_extra_path = ['_static/_headers']
 
 # -- Build settings ----------------------------------------------------------
 nitpicky = True  # Warn about broken references
